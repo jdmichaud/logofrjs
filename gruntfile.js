3a6efd8940869e7cea366752c97a80cabd3b22a3
JD <jeandaniel.michaud@ge.com>
2015-11-17 14:14:42 +0100

    Add concat and watch tasks

--- gruntfile.js
+++ gruntfile.js
@@ -2,14 +2,33 @@ module.exports = function(grunt) {
 	'use strict';
 
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			file: {
 				src: ['js/**/*.js']
 			}
+		},
+		concat: {
+			options: {
+				// define a string to put between each file in the concatenated output
+				separator: ';'
+			},
+			dist: {
+				// the files to concatenate
+				src: ['js/**/*.js'],
+				// the location of the resulting JS file
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		watch: {
+			files: ['<%= jshint.file.src %>'],
+			tasks: ['jshint']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'concat']);
 }
