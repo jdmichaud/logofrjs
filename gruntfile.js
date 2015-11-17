5a01564ee9db36e3e9a8c2c7e558ae04d9fb9dbc
JD <jeandaniel.michaud@ge.com>
2015-11-17 17:00:52 +0100

    Add uglify grunt tasks.

--- gruntfile.js
+++ gruntfile.js
@@ -9,15 +9,28 @@ module.exports = function(grunt) {
 			}
 		},
 		concat: {
-			options: {
-				// define a string to put between each file in the concatenated output
-				separator: ';'
-			},
 			dist: {
 				// the files to concatenate
 				src: ['js/**/*.js'],
 				// the location of the resulting JS file
-				dest: 'dist/<%= pkg.name %>.js'
+				dest: 'dist/js/<%= pkg.name %>.js'
+			},
+			options: {
+				// define a string to put between each file in the concatenated output
+				separator: ';',
+				// Setup a source map for easier debugging
+				sourceMap: true
+			},
+		},
+		uglify: {
+			dist: {
+				files: {
+					'dist/js/<%= pkg.name %>.min.js': '<%= concat.dist.dest %>'
+				},
+				options: {
+					sourceMap: true,
+					sourceMapIn: '<%= concat.dist.dest %>.map'
+				}
 			}
 		},
 		watch: {
@@ -28,7 +41,8 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint', 'concat']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 }
