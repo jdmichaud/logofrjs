436e985df8c9fb4c99290fc4a99ea57c61a71186
JD <jeandaniel.michaud@ge.com>
2015-11-20 14:14:35 +0100

    reworked gruntfile for FE preparation. Still in progress...

--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,71 @@
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			file: {
-				src: ['js/**/*.js']
-			},
-			options: {
-				multistr: true,
-			}
-		},
-		concat: {
-			dist: {
-				// the files to concatenate
-				src: ['js/**/*.js'],
-				// the location of the resulting JS file
-				dest: 'dist/js/<%= pkg.name %>.js'
-			},
-			options: {
-				// define a string to put between each file in the concatenated output
-				separator: ';',
-				// Setup a source map for easier debugging
-				sourceMap: true
-			},
-		},
-		uglify: {
-			dist: {
-				files: {
-					'dist/js/<%= pkg.name %>.min.js': '<%= concat.dist.dest %>'
-				},
-				options: {
-					sourceMap: true,
-					sourceMapIn: '<%= concat.dist.dest %>.map'
-				}
-			}
-		},
-		requirejs: {
-			dist: {
-				options: {
-					baseUrl: 'js',
-					out: 'dist/js/app.js',
-					include: 'logofr'
-				}
-			}
-		},
-		watch: {
-			files: ['<%= jshint.file.src %>'],
-			tasks: ['jshint']
-		}
-	});
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      file: {
+        src: [
+          'js/**/*.js',
+          '!js/logo-grammar.js' // jshint does not manage template string yet
+        ]
+      },
+      options: {
+        multistr: true,
+      }
+    },
+    concat: {
+      dist: {
+        // the files to concatenate
+        src: ['js/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'dist/js/<%= pkg.name %>.js'
+      },
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';',
+        // Setup a source map for easier debugging
+        sourceMap: true
+      },
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/js/<%= pkg.name %>.min.js': '<%= concat.dist.dest %>'
+        },
+        options: {
+          sourceMap: true,
+          sourceMapIn: '<%= concat.dist.dest %>.map'
+        }
+      }
+    },
+    requirejs: {
+      dist: {
+        options: {
+          baseUrl: '.',
+          out: 'dist/js/app.js',
+          include: 'js/logofr',
+          name: 'node_modules/almond/almond',
+          inlineText: true,
+          findNestedDependencies: true,
+          optimize: 'ulgify'
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.file.src %>'],
+      tasks: ['jshint']
+    },
+    clean: { dist: [ 'dist' ] }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'requirejs']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'requirejs']);
+  //grunt.registerTask('default', ['jshint', 'requirejs']);
 }
