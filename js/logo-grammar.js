70e102061f799836e17a6e834044401a57168e15
JD <jeandaniel.michaud@ge.com>
2015-11-20 14:16:08 +0100

    using back quotes from grammar

--- js/logo-grammar.js
+++ js/logo-grammar.js
@@ -4,32 +4,58 @@
 
 define(function() {
 
-	var astTypeEnum = {
-		INSTRUCTION: 0,
-		PROGRAM: 1
-	};
+  var astTypeEnum = {
+    INSTRUCTION: 0,
+    PROGRAM: 1
+  };
 
-	return {
-		grammar: '\
-			keyword = [A-Za-z]+ \
-			integer = [0-9]+ \
-			// Matches single-line comments \
-			comment = "//" (!lb .)* \
-			ws = [ \t ] // Whitespaces \
-			// Matches any number of whitespace/comments in a row \
-			_  = (ws / comment)* \
-			// newlines are our only instruction separator \
-			newline = [\n\r] \
-			// Define what is an instruction \
-			instruction "instruction" = k:keyword newline | k:keyword digits:integer newline { \
-				type: astTypeEnum.INSTRUCTION, \
-	 			command: k.join(""), \
-				arg: parseInt(digits.join(""), 10) \
-			} \
-			program "program" = instructions * { \
-				type: astTypeEnum.PROGRAM, \
-				instrutions: instructions \
- 			} \
-		'
-	};
+  return {
+    grammar:
+`
+program "program" =
+  ( instruction newline ) *
+//    return {
+//      type: "PROGRAM",
+//      instrutions: i
+//    };
+//  }
+
+keyword =
+  [A-Za-z]+
+
+integer =
+  [0-9]+
+
+// Whitespaces
+ws =
+  [ \\t  ]
+
+// Matches any number of whitespace/comments in a row
+_  =
+  (ws)*
+
+// newlines are our only instruction separator
+newline =
+  [\\n\\r\\u2028\\u2029]
+
+sourceCharacter
+  = .
+
+comment
+  = "//" (!newline sourceCharacter)*
+
+// Define what is an instruction
+instruction "instruction" =
+   k:keyword _ digits:integer
+  / k:keyword _
+  / comment
+  / newline
+//    return {
+//      type: "INSTRUCTION",
+//      command: k.join(""),
+//      arg: parseInt(digits.join(""), 10)
+//    };
+//  }
+`
+  };
 });
