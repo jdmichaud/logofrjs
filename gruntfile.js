11ac985e2a513b53f0fae723de8145e363203991
jd <jean.daniel.michaud@gmail.com>
2015-11-26 21:51:40 +0100

    Move karma conf file.

--- gruntfile.js
+++ gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js'
+        configFile: 'test/karma.conf.js'
       }
     },
     watch: {
