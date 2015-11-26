cee55538daa2ad38d043c023ac81d6ba351ef4c4
jd <jean.daniel.michaud@gmail.com>
2015-11-26 21:49:57 +0100

    Add test for mirobot service.

---
+++ test/microbot-service.spec.js
@@ -0,0 +1,29 @@
+define(['../js/microbot-service.js'], function (microbotService) {
+  'use strict';
+
+  // connect
+  // send
+  // on connection close
+  // double send
+
+  describe('Service test', function () {
+    it('on call to connect, mirobotService shall connect to a web socket server specified by a addresse and a port', function () {
+    });
+
+    it('upon opening of the connection mirobotService shall call a provided handler', function () {
+    });
+
+    it('upon closing of the connection mirobotService shall call a provided handler', function () {
+    });
+
+    it('on call to send, mirobotService shall be able to send a message', function () {
+    });
+
+    it('on error from the web socket serveur, mirobotService shall call a provided error handler', function () {
+    });
+
+    it('mirobotService shall be able to send several message synchronously', function () {
+    });
+
+  });
+});
