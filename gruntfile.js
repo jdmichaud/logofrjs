4fe36d9e666d26823c4fc519e9924ea355c3a41e
JD <jeandaniel.michaud@ge.com>
2015-12-02 11:35:23 +0100

    Fix gruntfile by removing the dependency to browserify.

--- gruntfile.js
+++ gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-browserify');
+  //grunt.loadNpmTasks('grunt-browserify');
 
   grunt.registerTask('default', ['clean', 'jshint']);
   //grunt.registerTask('default', ['jshint', 'requirejs']);
