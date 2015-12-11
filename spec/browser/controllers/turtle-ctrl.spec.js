7104dfc85ee84c6733a0d1c4c865cc7c99a7bfbc
JD <jeandaniel.michaud@ge.com>
2015-12-11 14:52:00 +0100

    Add empty tests for controllers and services.

---
+++ spec/browser/controllers/turtle-ctrl.spec.js
@@ -0,0 +1,9 @@
+define(['../../../app/js/controllers/turtle-ctrl.js'], function (TurtleCtrl) {
+  'use strict';
+
+  describe('Turtle controller', function () {
+    it('shall initialize its scope for errors, turtle message', function () {
+    });
+  });
+
+});
