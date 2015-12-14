737fa8cd956331cda4b6f2f18b4d4ae439f2b078
jd <jean.daniel.michaud@gmail.com>
2015-12-14 21:12:00 +0100

    Add a test for one line instructions and update parser specs.

--- spec/unit/parser.spec.js
+++ spec/unit/parser.spec.js
@@ -3,13 +3,22 @@ require(['fs', 'pegjs', 'parser'], function (fs, PEG, parser) {
 
   describe('Parser', function () {
     var testFile = [
+      { filename: 'logo/ko-instr-no-newline.logo',   expectedOutcome: 1 },
+      { filename: 'logo/ko-missing-argument.logo',   expectedOutcome: 0 }, // Catched by the syntax checker
+      { filename: 'logo/ko-negative-argument.logo',  expectedOutcome: 1 },
+      { filename: 'logo/ko-repeat-no-arg.logo',      expectedOutcome: 1 },
+      { filename: 'logo/ko-two-arguments.logo',      expectedOutcome: 1 },
       { filename: 'logo/ko.logo',                    expectedOutcome: 0 }, // Catched by the syntax checker
       { filename: 'logo/ko2.logo',                   expectedOutcome: 0 }, // Catched by the syntax checker
-      { filename: 'logo/ko-negative-argument.logo',  expectedOutcome: 1 },
-      { filename: 'logo/ko-two-arguments.logo',      expectedOutcome: 1 },
+      { filename: 'logo/ok-bracket-instr.logo',      expectedOutcome: 0 },
+      { filename: 'logo/ok-calibrate.logo',          expectedOutcome: 0 },
       { filename: 'logo/ok-comment.logo',            expectedOutcome: 0 },
+      { filename: 'logo/ok-instr-no-newline.logo',   expectedOutcome: 0 },
+      { filename: 'logo/ok-maison.logo',             expectedOutcome: 0 },
+      { filename: 'logo/ok-repeat.logo',             expectedOutcome: 0 },
       { filename: 'logo/ok-simple.logo',             expectedOutcome: 0 },
       { filename: 'logo/ok-simple2.logo',            expectedOutcome: 0 },
+      { filename: 'logo/ok-square.logo',             expectedOutcome: 0 },
       { filename: 'logo/ok-ultra-simple.logo',       expectedOutcome: 0 }
     ];
 
